bc2b0b24693bed3379bb1f282bcdbbfa2d384d52
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-12-02 09:38:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int numero;
+    cin>>numero;
+    if (funzione(numero,2)==1)
+        cout<<"numero primo";
+    else cout<<"numero non primo";
+    return 0;
 }
+Footer
+© 2022 GitHub, In
